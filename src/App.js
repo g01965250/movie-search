5fe12ce8bf97eb005ed27e60ffcbc5548018ad20
Orton <g01965250@gmail.com>
2026-04-09 01:33:30 +0800

    feat: display popular movie list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
-
+import useFetch from './useFetch';
 function App() {
+  const { data, loading, error } = useFetch(
+    'https://api.themoviedb.org/3/movie/popular?language=zh-TW&page=1',
+  );
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error:{error}</div>;
+  console.log(data);
+
   return (
     <div>
       <h1>Movie Search</h1>
+      <div>
+        {data &&
+          data.results.map((movie) => (
+            <div key={movie.id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <h3>{movie.title}</h3>
+              <p>評分：{movie.vote_average}</p>
+              <p>上映日期：{movie.release_date}</p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
---
+++ src/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(url, {
+          headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.REACT_APP_TMDB_TOKEN}`,
+          },
+        });
+        if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+        const json = await res.json();
+        setData(json);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
