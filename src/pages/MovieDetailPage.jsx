0d03cb172754679a6039b8b2f33515bad609ae35
Orton <g01965250@gmail.com>
2026-04-18 02:05:56 +0800

    feat: add TMDB attribution footer

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -67,6 +67,18 @@ function MovieDetailPage() {
           </div>
         </div>
       )}
+      <footer className="footer">
+        <p>
+          This product uses the TMDB API but is not endorsed or certified by
+          TMDB.
+        </p>
+        <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">
+          <img
+            src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+            alt="TMDB"
+          />
+        </a>
+      </footer>
     </div>
   );
 }
